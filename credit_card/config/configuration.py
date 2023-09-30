550f040e5e5ce3a4f9c135750dbe369d37ef5a95
Manan <manan511shah@gmail.com>
2023-09-30 15:33:48 +0530

    Data Transform configuration completed and tested

--- credit_card/config/configuration.py
+++ credit_card/config/configuration.py
@@ -2,7 +2,7 @@ import os,sys
 from credit_card.logger import logging
 from credit_card.exception import CreditCardException
 from credit_card.constant import *
-from credit_card.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from credit_card.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformConfig
 from credit_card.util.util import read_yaml
 
 class Configuration:
@@ -73,6 +73,37 @@ class Configuration:
             logging.info(f"data validation config : {data_validation_config}")
             return data_validation_config
 
+        except Exception as e:
+            raise CreditCardException(e,sys) from e
+        
+    def get_data_transform_config(self)->DataTransformConfig:
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            data_transform_config = self.config_info[DATA_TRANSFORM_CONFIG_KEY]
+
+            data_transform_config_dir = os.path.join(artifact_dir,DATA_TRANSFORM_DIR,self.time_stamp)
+
+            data_transform_graph_dir = os.path.join(data_transform_config_dir,data_transform_config[DATA_TRANSFORM_GRAPH_DIR_KEY])
+
+            data_transform_tain_dir = os.path.join(data_transform_config_dir,data_transform_config[DATA_TRANSFORM_TRAIN_DIR_KEY])
+
+            data_transform_test_dir = os.path.join(data_transform_config_dir,data_transform_config[DATA_TRANSFORM_TEST_DIR_KEY])
+
+            data_transform_preprocessed_dir = os.path.join(data_transform_config_dir,data_transform_config[DATA_TRANSFORM_PREPROCESSED_OBJECT_DIR_KEY],
+                                                           data_transform_config[DATA_TRANSFORM_PREPROCESSED_OBJECT_FILE_NAME_KEY])
+            
+            data_transform_cluster_dir = os.path.join(data_transform_config_dir,data_transform_config[DATA_TRANSFORM_CLUSTER_MODEL_DIR_KEY],
+                                                      data_transform_config[DATA_TRANSFORM_CLUSTER_MODEL_NAME_KEY])
+            
+            data_transform_config =  DataTransformConfig(transform_train_dir=data_transform_tain_dir,
+                                                         transform_test_dir=data_transform_test_dir,
+                                                         graph_save_dir=data_transform_graph_dir,
+                                                         preprocessed_file_path=data_transform_preprocessed_dir,
+                                                         cluster_model_file_path=data_transform_cluster_dir)
+            logging.info(f"data transform config : {data_transform_config}")
+
+            return data_transform_config
         except Exception as e:
             raise CreditCardException(e,sys) from e
         
